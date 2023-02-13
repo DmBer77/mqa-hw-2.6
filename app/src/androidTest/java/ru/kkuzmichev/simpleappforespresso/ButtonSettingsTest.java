baae4fc1b9a5244ef0e3fc69990cb11e9f78fa15
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2023-02-13 17:46:23 +0300

    add tests (intent, idlingResources)

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ButtonSettingsTest.java
@@ -0,0 +1,108 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+
+public class ButtonSettingsTest {
+
+    @Rule
+//    public IntentsTestRule intentsTestRule =
+//            new IntentsTestRule(MainActivity.class);
+
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before // Выполняется перед тестами
+    public void registerIdlingResources() { //Подключаемся к “счетчику”
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After // Выполняется после тестов
+    public void unregisterIdlingResources() { //Отключаемся от “счетчика”
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
+    @Test
+    public void mainActivityButtonSettingsTest() throws InterruptedException {
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                0)));
+        overflowMenuButton.check(matches(isDisplayed()));
+        overflowMenuButton.perform(click());
+
+        ViewInteraction materialTextView = onView(
+                allOf(withId(androidx.core.R.id.title), withText("Settings"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(com.google.android.material.R.id.content),
+                                        0),
+                                0)));
+        materialTextView.check(matches(isDisplayed()));
+
+        Intents.init();
+        materialTextView.perform(click());
+
+//        Thread.sleep(15000);
+        intended(hasAction(Intent.ACTION_VIEW));
+        intended(hasData("https://www.google.com/"));
+
+        Intents.release();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
